ce0ea501567076f6268bc5263b7cc2b846442e07
Vinny D <vdurham@prideseals.com>
2022-02-21 14:27:59 -0500

    editable task and date

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,6 +75,37 @@ $(".list-group").on("blur", "textarea", function() {
   $(this).replaceWith(taskP);
 })
 
+$(".list-group").on("click", "span", function() {
+  var date = $(this)
+    .text()
+    .trim();
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+  $(this).replaceWith(dateInput);
+  dateInput.trigger("focus");
+})
+
+$(".list-group").on("blur", "input[type='text']", function() {
+  var date = $(this)
+    .val()
+    .trim();
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+  tasks[status][index].date = date;
+  saveTasks();
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+  $(this).replaceWith(taskSpan);
+})
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
