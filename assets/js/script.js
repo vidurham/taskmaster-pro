19f8650d720f230c715a68147f6b9f06a3b50c12
Vinny D <vdurham@prideseals.com>
2022-02-21 22:05:17 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,17 +118,19 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activate: function(event) {
-    console.log("activate", this);
+  activate: function(event, ui) {
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
   },
-  deactivate: function(event){
-    console.log("deactivate", this);
+  deactivate: function(event, ui){
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active")
   },
   out: function(event){
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active")
   },
   update: function(event) {
     var tempArr = [];
@@ -160,6 +162,13 @@ $("#trash").droppable({
     tolerance: "touch",
     drop: function(event, ui) {
       ui.draggable.remove();
+      $(".bottom-trash").removeClass("bottom-trash-active");
+    },
+    over: function(event, ui) {
+      $(".bottom-trash").addClass("bottom-trash-active");
+    },
+    out: function(event, ui) {
+      $(".bottom-trash").removeClass("bottom-trash-active");
     }
 })
 
@@ -174,6 +183,12 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 }
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -192,7 +207,7 @@ $("#modalDueDate").datepicker({
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
